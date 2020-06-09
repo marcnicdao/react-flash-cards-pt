66938f060428b3f3eaa6cf3ca959ad2ba2dc7b56
Marc Nicdao <nicdaom466@gmail.com>
2020-06-09 12:33:58 -0700

    Completed View All Cards Function

--- src/components/view-cards.jsx
+++ src/components/view-cards.jsx
@@ -5,9 +5,9 @@ export default class ViewCards extends Component {
         const { cards } = this.props
         const cardList = cards.map((card)=>{
             return(
-                <div className='col mb-4 card-group'>
+                <div key={card.question} className='col mb-4 card-group'>
                     <div className="rounded-lg card h-100 text-center bg-dark text-white m-2"
-                        key={card.question}>
+                        >
                         <h5>Question:</h5>
                         <p>{card.question}</p>
                         <div className="card-body bg-info">
